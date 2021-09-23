437c0dc992037f7df3574bafbe0de1b15b83a54d
isagulovurmat <falone641234@gmail.com>
2021-09-24 03:04:24 +0600

    second commit

--- courses/course/models.py
+++ courses/course/models.py
@@ -1,19 +1,11 @@
 from django.db import models
 
-class Category(models.Model):
-
-    name = models.CharField(max_length=150)
-    imgpath = models.CharField(max_length=150)
-
-    def __str__(self):
-        return self.name
-
-
 class Branch(models.Model):
 
     latitude = models.CharField(max_length=150)
     longitude = models.CharField(max_length=150)
     address = models.CharField(max_length=150)
+
 
     def __str__(self):
         return self.address
@@ -26,17 +18,26 @@ class Contact(models.Model):
     )
     status = models.IntegerField(choices=CHOICES)
 
+
     def __str__(self):
         return f'{self.status}'
+
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    imgpath = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.name
+
 
 class Course(models.Model):
 
     name = models.CharField(max_length=150)
     description = models.CharField(max_length=150)
-    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='category', null=True)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='category', null=True)
     logo = models.CharField(max_length=150)
-    contacts = models.OneToOneField(Contact, on_delete=models.CASCADE, related_name='contacts', null=True)
-    branches = models.OneToOneField(Branch, on_delete=models.CASCADE, related_name='branches', null=True)
+    contacts = models.ManyToManyField(Contact, related_name='contacts', null=True)
+    branches = models.ManyToManyField(Branch, related_name='branches', null=True)
 
     def __str__(self):
         return self.name
--- courses/course/serializers.py
+++ courses/course/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers, status
 from rest_framework.response import Response
-
 from .models import *
 
 
@@ -8,7 +7,6 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = ('id', 'name', 'imgpath')
-
 
 class BranchSerializer(serializers.ModelSerializer):
     class Meta:
@@ -23,21 +21,26 @@ class ContactSerializer(serializers.ModelSerializer):
 
 
 class CourseSerializer(serializers.ModelSerializer):
+
     category = CategorySerializer()
-    contacts = ContactSerializer()
-    branches = BranchSerializer()
+    contacts = ContactSerializer(many=True)
+    branches = BranchSerializer(many=True)
 
     class Meta:
         model = Course
-        fields = ['name', 'description', 'category', 'logo', 'contacts', 'branches']
+        fields = ['name', 'description','logo', 'category', 'contacts', 'branches']
 
     def create(self, validated_data):
         category_data = validated_data.pop('category')
         category = Category.objects.create(**category_data)
         contacts_data = validated_data.pop('contacts')
-        contacts = Contact.objects.create(**contacts_data)
+        # contacts = Contact.objects.create(**contacts_data)
         branches_data = validated_data.pop('branches')
-        branches = Branch.objects.create(**branches_data)
-        course = Course.objects.create(category=category, contacts=contacts, branches=branches, **validated_data)
+        # branches = Branch.objects.create(**branches_data)
+        course = Course.objects.create(**validated_data)
+        for contact_data in contacts_data:
+            Contact.objects.create(course=course, **contact_data)
+        for branch_data in branches_data:
+            Branch.objects.create(course=course, **branch_data)
         return course
 
